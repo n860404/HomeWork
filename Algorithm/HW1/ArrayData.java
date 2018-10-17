6d4a3ae271af78355179768ad9d054d24324fe1e
n860404 <30925220+n860404@users.noreply.github.com>
2018-10-18 00:00:52 +0800

    Create ArrayData.java

---
+++ Algorithm/HW1/ArrayData.java
@@ -0,0 +1,8 @@
+public abstract class ArrayData {
+	double[] A;
+	public abstract double max();
+	public abstract double avg();
+	public abstract void reverse();
+	public abstract void shuffle();
+	public abstract double inner_product(double[] B);
+}
